de9150af369f2bc3f8a7bd0f15e9bdc53f71c0bd
baardl <bard.lind@gmail.com>
2015-12-16 09:51:55 +0100

    Added main functionallity for clearity.

--- src/main/java/com/bardlind/pattern/cor/Main.java
+++ src/main/java/com/bardlind/pattern/cor/Main.java
@@ -3,6 +3,14 @@ package com.bardlind.pattern.cor;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        EmailClient client = new EmailClient();
+        Email email = new Email("bard.lind@gmail.com");
+        client.emailReceived(email);
+        email = new Email("dill@businessaddress.com");
+        client.emailReceived(email);
+        email = new Email("bard.lind@example.com");
+        client.emailReceived(email);
+        email = new Email("bbbb");
+        client.emailReceived(email);
     }
 }
